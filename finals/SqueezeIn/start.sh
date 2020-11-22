ebc3d794128a3fe47468da13afd1eb211049e5a8
Luis Noriega <luisnoriegag94@gmail.com>
2020-11-22 16:19:42 -0600

    Update start.sh

--- finals/SqueezeIn/start.sh
+++ finals/SqueezeIn/start.sh
@@ -2,3 +2,4 @@ export PORT=3142
 
 docker build -t squeezein .
 docker run --rm -itd -e PORT=$PORT -p 3142:$PORT squeezein
+docker exec -it $(docker ps -aqf "ancestor=squeezein") chmod +x /usr/local/bin/geckodriver
